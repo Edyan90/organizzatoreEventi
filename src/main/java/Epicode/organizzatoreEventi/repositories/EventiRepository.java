aef4dc4404e89655c9ea374f75beb81b276f424b
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-09-20 12:45:16 +0200

    annotazione mancante aggiunta

--- src/main/java/Epicode/organizzatoreEventi/repositories/EventiRepository.java
+++ src/main/java/Epicode/organizzatoreEventi/repositories/EventiRepository.java
@@ -2,8 +2,10 @@ package Epicode.organizzatoreEventi.repositories;
 
 import Epicode.organizzatoreEventi.entities.Evento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface EventiRepository extends JpaRepository<Evento, UUID> {
 }
