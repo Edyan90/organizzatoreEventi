66aebeb841fd95d67407928a13bde73cd3488036
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-09-20 10:36:35 +0200

    Initial commit

---
+++ src/main/java/Epicode/organizzatoreEventi/OrganizzatoreEventiApplication.java
@@ -0,0 +1,13 @@
+package Epicode.organizzatoreEventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizzatoreEventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizzatoreEventiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Epicode/organizzatoreEventi/OrganizzatoreEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package Epicode.organizzatoreEventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizzatoreEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
