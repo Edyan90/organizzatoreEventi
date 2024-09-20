d7ad7c8e6b37ca10077526be9419298ba77a8302
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-09-20 12:21:52 +0200

    create metodi su utentiRepository

--- src/main/java/Epicode/organizzatoreEventi/repositories/UtentiRepository.java
+++ src/main/java/Epicode/organizzatoreEventi/repositories/UtentiRepository.java
@@ -3,7 +3,11 @@ package Epicode.organizzatoreEventi.repositories;
 import Epicode.organizzatoreEventi.entities.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UtentiRepository extends JpaRepository<Utente, UUID> {
+    Optional<Utente> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
