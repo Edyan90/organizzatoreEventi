b4685510d65daeead42d5cfd0e71a43e276416e0
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-09-20 12:44:49 +0200

    add annotazioni Repository

--- src/main/java/Epicode/organizzatoreEventi/repositories/UtentiRepository.java
+++ src/main/java/Epicode/organizzatoreEventi/repositories/UtentiRepository.java
@@ -2,10 +2,12 @@ package Epicode.organizzatoreEventi.repositories;
 
 import Epicode.organizzatoreEventi.entities.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UtentiRepository extends JpaRepository<Utente, UUID> {
     Optional<Utente> findByEmail(String email);
 
