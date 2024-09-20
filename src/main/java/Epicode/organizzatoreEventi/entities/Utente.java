6a3bd5ea4332b8cd537cf52e320e7c355747886c
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-09-20 12:44:33 +0200

    implementato UserDetails e i relativi override

--- src/main/java/Epicode/organizzatoreEventi/entities/Utente.java
+++ src/main/java/Epicode/organizzatoreEventi/entities/Utente.java
@@ -4,7 +4,11 @@ import Epicode.organizzatoreEventi.enums.UtenteType;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,7 +19,7 @@ import java.util.UUID;
 @Getter
 @NoArgsConstructor
 @ToString
-public class Utente {
+public class Utente implements UserDetails {
     @Id
     @GeneratedValue
     @Setter(AccessLevel.NONE)
@@ -36,6 +40,16 @@ public class Utente {
         this.email = email;
         this.password = password;
 
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.utenteType.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
     }
 
 }
